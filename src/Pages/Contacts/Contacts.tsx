6b35af5151199b160bfe4042baca7d6b9b7a6a62
MarianaDeAthayde <mariana.ozorio@ab-inbev.com>
2024-10-25 16:48:45 -0300

    add contacts

--- src/Pages/Contacts/Contacts.tsx
+++ src/Pages/Contacts/Contacts.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import styles from "./Contacts.module.css";
 
 function Contacts() {
-    return(
-        <p>test</p>
-    )
+  return (
+    <div className={styles.container}>
+      <ul className={styles.contactsWrapper}>
+        <li>
+          <a href="mailto:mariana.athayde@gmail.com" target="_blank">
+            me mande um <br></br>
+            <h3>email</h3>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/mariana-athayde-o/"
+            target="_blank"
+          >
+            me adicione no <br></br>
+            <h3>linkedin</h3>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/MarianaOzorio" target="_blank">
+            veja meu <br></br>
+            <h3>github</h3>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Contacts;
