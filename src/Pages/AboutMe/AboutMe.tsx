e5a955faff6a283a9ebb0f6b5f58c4c9bd098545
MarianaDeAthayde <mariana.ozorio@ab-inbev.com>
2024-10-25 16:55:06 -0300

    fix navbar and add about me temporary text

--- src/Pages/AboutMe/AboutMe.tsx
+++ src/Pages/AboutMe/AboutMe.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./AboutMe.module.css";
 
 function AboutMe() {
     return(
-        <p>test</p>
+        <article className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nemo cum quaerat nihil culpa molestias eius, adipisci aspernatur accusantium temporibus. Voluptatibus, sequi. Repudiandae error provident, perspiciatis sed dolor officiis laborum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, magni deserunt, officia distinctio, odio esse sint eveniet voluptatem iure nesciunt aperiam dolores placeat dignissimos quam quisquam consequuntur cupiditate vero velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nemo cum quaerat nihil culpa molestias eius, adipisci aspernatur accusantium temporibus. Voluptatibus, sequi. Repudiandae error provident, perspiciatis sed dolor officiis laborum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, magni deserunt, officia distinctio, odio esse sint eveniet voluptatem iure nesciunt aperiam dolores placeat dignissimos quam quisquam consequuntur cupiditate vero velit.</article>
     )
 }
 
