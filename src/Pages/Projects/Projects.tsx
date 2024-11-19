46404e845e6aea7de7da494edc346ddf0c6c77e7
MarianaDeAthayde <mariana.ozorio@ab-inbev.com>
2024-11-19 11:00:58 -0300

    add projects

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import styles from "./Projects.module.css";
+import projects from "../../local-json/projects.json";
 
 function Projects() {
-    return(
-        <p>test</p>
-    )
+  return (
+    <div className={styles.projectsList}>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.name}>
+            <h3>{project.name}</h3>
+            <br></br>
+            <a href={project.siteUrl}>site -</a>
+            <a href={project.codigoUrl}>código</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Projects;
